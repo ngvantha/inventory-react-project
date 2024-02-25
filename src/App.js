388135737be9b6326b1ec3b33d5f5f0a0381f1d7
ngvtha <nguyentha.jp@gmail.com>
2024-02-25 16:31:13 +0900

    add react router dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route, Routes } from 'react-router-dom'
+import { Suspense, lazy } from 'react';
+
+// Containers
+const DefaultLayout = lazy(() => import('./layout/DefaultLayout'))
+
+// Pages
+const Login = lazy(() => import('./views/pages/login/Login'))
+const Register = lazy(() => import('./views/pages/register/Register'))
+const Page404 = lazy(() => import('./views/pages/page404/Page404'))
+const Page500 = lazy(() => import('./views/pages/page500/Page500'))
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={<div className="container">Loading...</div>}>
+      <Routes>
+        <Route exact path="/login" name="Login Page" element={<Login />} />
+        <Route exact path="/register" name="Register Page" element={<Register />} />
+        <Route exact path="/404" name="Page 404" element={<Page404 />} />
+        <Route exact path="/500" name="Page 500" element={<Page500 />} />
+        <Route path="*" name="Home" element={<DefaultLayout />} />
+      </Routes>
+    </Suspense>
   );
 }
 
